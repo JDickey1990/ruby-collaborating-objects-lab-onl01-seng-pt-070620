b66056bdb8b8391d1210f6ecdd92aaac285af1af
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-05 01:49:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song 
   
  attr_accessor :name, :artist 
@@ -14,8 +15,8 @@ class Song
   end
   
   def self.new_by_filename(file)
-    file.each do |song|
       song = self.new
+      binding.pry
       song.artist = song.split(" - ")[0]
     song
     end
