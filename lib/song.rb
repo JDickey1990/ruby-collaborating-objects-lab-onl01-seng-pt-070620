7aa714b98877606dc451a7e7a4f1b856bc7c6f94
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-05 02:04:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,7 @@ class Song
   
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
-    song = self.new
-    song.name=song_info[1]
+    song = self.new(song_info[1])
     song.artist= song_info[0]
   end
   #binding.pry
